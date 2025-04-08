4245e0a136e7acc9b0c526f6237a4cd6fcfcf2fd
Davi0805 <melocardozo.video@gmail.com>
2025-04-09 00:42:46 +0100

    feat(UserRepository): Simple persistence layer

---
+++ src/Repositories/UserRepository.js
@@ -0,0 +1,22 @@
+const db = require('../config/Sqlite');
+// remember to use await in the queries
+
+
+// idk if we allowed to use ORM, so i will write it all guys
+class UserRepository {
+
+    // using '*' while theres still no frontend user interface defined
+    async findAll() {
+        return db.raw('SELECT * FROM users');
+    }
+
+    async findById(id) {
+        return db.raw('SELECT * FROM users WHERE id = ?', [id]);
+    }
+
+    async save(user) {
+        return db('users').insert(user);
+    }
+}
+
+module.exports = new UserRepository();
