0201b5b5a77f0a6b54836cd14ab7dd31be7828b2
Davi0805 <melocardozo.video@gmail.com>
2025-04-08 23:57:27 +0100

    feat(db-init.sql): script to create the tables

---
+++ src/config/db-init.js
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   db-init.js                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davi <davi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 23:32:49 by davi              #+#    #+#             */
+/*   Updated: 2025/04/08 23:52:50 by davi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const db = require('./Sqlite');
+const fs = require('fs'); // fstream equivalent
+
+// used the readFileSync cause the original ReadSync is async (non-blocking)
+const sql_scheme = fs.readFileSync('./init.sql', 'utf-8');
+
+// here i call our db instance
+// to read raw queries from init.sql
+db.raw(sql_scheme).then(() => {
+    console.log('happiness!');
+    process.exit(0);
+  }).catch(err => {
+    console.error('Sadness:', err);
+    process.exit(1);
+  });
