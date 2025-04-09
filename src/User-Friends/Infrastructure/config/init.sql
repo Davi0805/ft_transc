b52c991aceaf00c3dd67f1d413915526ece50bf9
Davi0805 <melocardozo.video@gmail.com>
2025-04-10 00:44:30 +0100

    feat(init.sql): friend requests table added

--- src/User-Friends/Infrastructure/config/init.sql
+++ src/User-Friends/Infrastructure/config/init.sql
@@ -15,3 +15,16 @@ CREATE TABLE users (
 -- i dont really know if this really work in sqlite, but u know
 -- WHY NOT?
 CREATE INDEX idx_username ON users (username);
+
+CREATE TABLE friend_requests (
+    request_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    from_user_id INTEGER NOT NULL,
+    to_user_id INTEGER NOT NULL,
+    status VARCHAR(10) NOT NULL CHECK (status IN ('PENDING', 'ACCEPTED', 'REJECTED')),
+
+    FOREIGN KEY (from_user_id) REFERENCES users(user_id),
+    FOREIGN KEY (to_user_id) REFERENCES users(user_id),
+
+    UNIQUE(from_user_id, to_user_id), -- checks to avoid duplicated rows
+    CHECK (from_user_id != to_user_id) -- if someone just sends a friend request to yourself
+);
