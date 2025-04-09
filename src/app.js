397165f9db5131cb13ddcc1d0efb28f767943957
Davi0805 <melocardozo.video@gmail.com>
2025-04-09 01:46:38 +0100

    feat(app): simple fastify config

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const Fastify = require('fastify');
+const userRoutes = require('./routes/UserRoutes');
+const sensible = require('fastify-sensible');
+
+const setup = () => {
+    const app = Fastify({ logger: true });
+
+    app.register(userRoutes);
+    app.register(sensible);
+
+    return app;
+}
+
+const run = () => {
+    const app = setup();
+    try {
+        app.listen({ port: 8080});
+        console.log("Listening...");
+    } catch (error) {
+        console.log("FATAL");
+        process.exit(1);
+    }
+}
+
+run();
