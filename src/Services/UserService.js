a870a180e589675652528fcfa93babe5f9b031e9
Davi0805 <melocardozo.video@gmail.com>
2025-04-09 04:20:48 +0100

    feat(UserService): More methods added

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,4 +1,5 @@
 const userRepository = require('../Repositories/UserRepository');
+const bcrypt = require('bcrypt'); // hashing lib to the passwords
 
 // dont forget the infinite await/s in the returns and controller 
 // to work sync (blocking)
@@ -7,6 +8,33 @@ class UserService {
 
     async getAll() {
         return await userRepository.findAll();
+    }
+
+    async findById(id)
+    {
+        return await userRepository.findById(id);
+    }
+
+    async createUser(User) {
+        User.password_hash = await bcrypt.hash(User.password_hash, 10);
+        
+        try {
+            const result = await userRepository.save(User);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async Login(User)
+    {
+        const result = await userRepository.findByUsername(User.username);
+        try {
+            await bcrypt.compare(User.password, result[0].password_hash);
+            return true;           
+        } catch (error) {
+            return false;
+        }
     }
 };
 
