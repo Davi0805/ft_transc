43df8c96e5b2937bbbb08e648d2e7cae4f1c79d0
Davi0805 <melocardozo.video@gmail.com>
2025-04-09 17:01:40 +0100

    feat(RedisService): Bearer token substring added

--- src/Services/RedisService.js
+++ src/Services/RedisService.js
@@ -3,13 +3,12 @@ const sessionRepository = require('../Repositories/SessionRepository');
 class RedisService {
     async saveSession(token, metadata)
     {
-        console.log(token);
         await sessionRepository.save(token, metadata);
     }
 
     async getSession(token)
     {
-        const metadata = await sessionRepository.findByJwt(token);
+        const metadata = await sessionRepository.findByJwt(token.substring(7));
         return metadata;
     }
 }
