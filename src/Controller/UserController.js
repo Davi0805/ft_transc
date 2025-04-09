373d050df7327c4d7ed6dc38fa1ab48eaa037237
Davi0805 <melocardozo.video@gmail.com>
2025-04-09 04:22:14 +0100

    feat(inbound): More endpoints added

--- src/Controller/UserController.js
+++ src/Controller/UserController.js
@@ -1,11 +1,37 @@
 const userService = require('../Services/UserService');
 
+
+// todo: learn how to check this fucking structured | no structured classes
 class UserController {
 
     async getAll(req, reply)
     {
         const users = await userService.getAll();
         reply.send(users);
+    }
+
+    async getById(req, reply)
+    {
+        const user = await userService.findById(req.params.id);
+        reply.send(user);
+    }
+
+    async createUser(req, reply) 
+    {
+        const user = req.body;
+        const result = await userService.createUser(user);
+        if (!result)
+            return reply.code(400).send();
+        return reply.code(201);
+    }
+
+    async Login(req, reply)
+    {
+        const user = req.body;
+        const IsAuth = await userService.Login(user);
+        if (!IsAuth)
+            return reply.code(404).send();
+        reply.send();
     }
 }
 
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,6 +2,9 @@ const userController = require('../Controller/UserController');
 
 async function userRoutes(fastify, options) {
     fastify.get('/users', userController.getAll);
+    fastify.get('/users/:id', userController.getById);
+    fastify.post('/users', userController.createUser);
+    fastify.post('/login', userController.Login);
 }
 
 module.exports = userRoutes;
