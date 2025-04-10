5aba7b7550c4ee719f2799ab4dcd465472d0cff2
Davi0805 <melocardozo.video@gmail.com>
2025-04-10 02:24:30 +0100

    feat(FriendRequest): Service and repo created

--- src/User-Friends/Adapters/outbound/Repositories/FriendRequestRepository.js
+++ src/User-Friends/Adapters/outbound/Repositories/FriendRequestRepository.js
@@ -7,6 +7,12 @@ class FriendRequestRepository {
         return await db.raw('SELECT * FROM friend_requests');
     }
 
+    async newRequest(sender_id, receiver_id) {
+        return await db.raw('INSERT INTO friend_requests '+
+            '(from_user_id, to_user_id, status) VALUES (?, ?, ?)',
+             [sender_id, receiver_id, 'PENDING']);
+    }
+
 }
 
-module.exports = FriendRequestRepository;
+module.exports = new FriendRequestRepository();
--- src/User-Friends/Application/Services/FriendRequestService.js
+++ src/User-Friends/Application/Services/FriendRequestService.js
@@ -1,2 +1,15 @@
 const friendRequestRepo = require('../../Adapters/outbound/Repositories/FriendRequestRepository');
 
+class FriendRequestService {
+    async findAll()
+    {
+        return await friendRequestRepo.getAll();
+    }
+
+    async newRequest()
+    {
+        return await friendRequestRepo.newRequest();
+    }
+}
+
+module.exports = new FriendRequestService();
