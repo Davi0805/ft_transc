d36d090674e86a43ea3d8a7dc9d46d6c4112568a
Davi0805 <melocardozo.video@gmail.com>
2025-04-10 15:35:52 +0100

    feat(FriendsCacheRepo): Class created to cache Friends in redis using Redis Hash indexing

---
+++ src/User-Friends/Adapters/outbound/Repositories/FriendsCacheRepository.js
@@ -0,0 +1,23 @@
+const redis = require('../../../Infrastructure/config/Redis');
+
+class FriendsCacheRepository {
+    async save(user_id, metadata)
+    {
+        console.log("FRIENDS CACHE REPO = ", user_id + ' | METADATA = ' + JSON.stringify(metadata));
+        
+        metadata.forEach((item, index) => {
+            redis.hSet(`friends:${user_id}`, `friend_id:${item.user_id}`, JSON.stringify(item));
+        });
+        
+        await redis.expire(`friends:${user_id}`, 3600);
+    }
+
+    async get(user_id)
+    {
+        const result = await redis.hGetAll(`friends:${user_id}`);
+        const metadata = JSON.stringify(result);
+        return Object.keys(metadata).length > 2 ? metadata : null;
+    }
+}
+
+module.exports = new FriendsCacheRepository();
