32d7a61d1c46f944b6abddc898c1084a1ac81c1f
Davi0805 <melocardozo.video@gmail.com>
2025-04-10 16:37:35 +0100

    feat(SQL SCHEME): SQL tables created

---
+++ src/Chat/Infrastructure/config/init.sql
@@ -0,0 +1,24 @@
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE conversations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user1_id INTEGER NOT NULL,
+    user2_id INTEGER NOT NULL
+);
+
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    conversation_id INTEGER NOT NULL,
+    from_user_id INTEGER NOT NULL,
+    message_content TEXT NOT NULL,
+    metadata TEXT, -- maybe i can use to save match urls or tokens for match invites
+    
+    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id)
+);
+
+
+CREATE INDEX idx_conversation_id ON conversations(id);
+CREATE INDEX idx_messages_from_user_id ON messages(from_user_id);
+ -- idk if necessary
+CREATE INDEX idx_messages_conversation_id ON messages(conversation_id);
