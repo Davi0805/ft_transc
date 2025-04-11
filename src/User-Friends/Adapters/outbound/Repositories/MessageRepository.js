9ccbb0578a2c684763f26c48cd4cc42c43e3f008
Davi0805 <melocardozo.video@gmail.com>
2025-04-11 19:33:59 +0100

    fix(MessageRepo): idk if its change something but i convert to json the data

--- src/User-Friends/Adapters/outbound/Repositories/MessageRepository.js
+++ src/User-Friends/Adapters/outbound/Repositories/MessageRepository.js
@@ -4,8 +4,10 @@ class MessageRepository {
 
     async send(topic, message)
     {
-        const messageArray = Object.entries(message).flat();
-        await redis.XADD(topic, '*', ...messageArray);
+        /* const messageArray = Object.entries(message).flat(); */
+        /* await redis.XADD(topic, '*', ...messageArray); */
+        console.log('JSON MANDADO - ' + JSON.stringify(message));
+        await redis.XADD(topic, '*', JSON.stringify(message));
     }
 
 }
