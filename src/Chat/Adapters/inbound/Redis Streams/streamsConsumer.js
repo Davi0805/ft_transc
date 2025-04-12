d87fce37cf8cac24623bcde51d3f29610d17f68c
Davi0805 <melocardozo.video@gmail.com>
2025-04-12 14:24:18 +0100

    feat(StreamConsumer): new friend logic when user is online

--- src/Chat/Adapters/inbound/Redis Streams/streamsConsumer.js
+++ src/Chat/Adapters/inbound/Redis Streams/streamsConsumer.js
@@ -1,8 +1,11 @@
-const redis = require('../../../Infrastructure/config/Redis');
+const redis = require('../../../Infrastructure/config/RedisStream');
 const conversationService = require('../../../Application/Services/ConversationsService');
+const connectedUsersService = require('../../../Application/Services/ConnectionsService');
 
 async function consumeNewFriendsEvent()
 {
+
+  // todo: REFAC WITH CONSUMER GROUP
     while(true)
     {
         const stream = await redis.xRead([
@@ -15,7 +18,7 @@ async function consumeNewFriendsEvent()
         });
 
 
-        // todo: try to find out why da fuck is need all of this
+        // todo: try to find out why da fuck is needed all of this
         // todo: just to desserialize from redis streams, and for real
         // todo: what a trash documentation for the redis streams to node js
         if (stream && stream.length > 0) {
@@ -30,7 +33,19 @@ async function consumeNewFriendsEvent()
                 try {
                   const data = JSON.parse(characters);
                   console.log('IS IT WORKING ???? | USERID1 = ' + data.user1 + ' | USERID2 = ' + data.user2);
+                  
+                  // todo: modify the type of data that is been stored in redis, cause
+                  // todo: on that you never know what type it is
+                  const user1Socket = await connectedUsersService.getUser(String(data.user1));
+                  if (user1Socket) {
+                    await user1Socket.send(JSON.stringify({ conversation_id: '<ID DA CONVERSA>',
+                      message: 'You now can talk with user ' + data.user2, metadata: 'newConversation'})); 
+                  }
+                  
+                  // todo: query should return the new conversation_id
                   await conversationService.save(data.user1, data.user2);
+                  
+                  
                 } catch (err) {
                   console.error('Failed to handle message:', err);
                 }
