cd1aa175740effd882e0071513448d9feb72fb76
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-05-16 22:32:45 +0100

    Added log in wrapper and foundation for the header too

--- src/Frontend/js/pages/login.js
+++ src/Frontend/js/pages/login.js
@@ -1,15 +1,33 @@
 export const LoginPage = {
-    template() {
-        return `
-            <div class="login">
-                <input type="text" placeholder="username" name="username" id="user">
-                <input type="password" name="password" placeholder="password" id="pass">
-                <button class="btn">Log In</button>
-            </div>
-        `;
-    }, 
+  template() {
+    return `
+      <div class="loggin-wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div class="input-box">
+            <input type="text" placeholder="Username" required />
+            <img src="../Assets/icons/user-square.png" />
+          </div>
+          <div class="input-box">
+            <input type="password" placeholder="Password" required />
+            <img src="../Assets/icons/lock.png" />
+          </div>
 
-    init () {
-        console.log("Login Page loaded!");
-    }
+          <div class="forgot">
+            <a href="#">Forgot password?</a>
+          </div>
+
+          <button type="submit" class="btn">Login</button>
+
+          <div class="register-link">
+            <p>Don't have an account? <a href="#">Register</a></p>
+          </div>
+        </form>
+      </div>
+        `;
+  },
+
+  init() {
+    console.log("Login Page loaded!");
+  },
 };
