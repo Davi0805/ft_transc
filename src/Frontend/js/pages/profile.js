6290fa3a0bbea74a65d11e8054a7968ebf92bc06
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-05-17 15:47:38 +0100

    frontend: added Profile and Register pages with routing support

---
+++ src/Frontend/js/pages/profile.js
@@ -0,0 +1,14 @@
+export const ProfilePage = {
+  template() {
+    return `
+      <div>
+          <h1>Profile</h1>
+          <p>This is the Home Page</p>
+      </div>
+      `;
+  },
+
+  init() {
+    console.log("Profile page loaded!");
+  },
+};
---
+++ src/Frontend/js/pages/register.js
@@ -0,0 +1,14 @@
+export const RegisterPage = {
+  template() {
+   return `
+    <div>
+      <h1>Register</h1>
+      <p>This is the Home Page</p>
+    </div>
+      `;
+  },
+
+  init() {
+    console.log("Register page loaded!");
+  },
+};
