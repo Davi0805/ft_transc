71a66b8d23dedb520532f6aedf32769eabdf11fc
Davi0805 <melocardozo.video@gmail.com>
2025-06-07 15:53:48 +0100

    doc(User-Friends): improving documentation and readability

--- src/User-Friends/Adapters/inbound/http/FriendRequestController.js
+++ src/User-Friends/Adapters/inbound/http/FriendRequestController.js
@@ -4,9 +4,10 @@ const redisService = require('../../../Application/Services/RedisService');
 
 class FriendRequestController {
 
+
     /* 
-        Endpoint for debug purposes
-        GET - localhost:8080/friend_requests
+    *    Endpoint for DEBUG purposes
+    *    GET - localhost:8080/friend_requests
     */
     async getAll(req, reply)
     {
@@ -14,11 +15,11 @@ class FriendRequestController {
         return reply.send(requests);   
     }
 
+
     /* 
-        Endpoint to create/start/request a new friendship
-        POST - localhost:8080/friend_requests
-        @params {HTTP Header} Authorization: Bearer <JWT> 
-        @params {sender_id: (Long), receiver_id: (Long)}
+    *    Endpoint to create/start/request a new friendship
+    *    POST - localhost:8080/friend_requests
+    *    @params {sender_id: (Long), receiver_id: (Long)}
     */
     async create(req, reply)
     {
@@ -27,46 +28,51 @@ class FriendRequestController {
         return reply.send();        
     }
 
+
     /* 
-        Endpoint to list all friend requests pending that u can accept
-        GET - localhost:8080/friend_requests/pending
-        @params {HTTP Header} Authorization: Bearer <JWT> 
+    *    Endpoint to list all friend requests pending that u can accept
+    *    GET - localhost:8080/friend_requests/pending
     */
-    async listPendingRequests(req, reply) {
+    async listPendingRequests(req, reply)
+    {
         const pendingRequests = await friendRequest.listPendingRequests(req.session.user_id);
         return reply.send(pendingRequests);   
     }
 
+
     /* 
-        Endpoint to accept the friendship requests that u received
-        POST - localhost:8080/friend_requests/{request_id}/accept
-        @params {HTTP Header} Authorization: Bearer <JWT> 
-        @params request_id: (Long) - id of friend_request
+    *    Endpoint to accept the friendship requests that u received
+    *    POST - localhost:8080/friend_requests/{request_id}/accept
+    *    @params request_id: (Long) - id of friend_request
     */
-    async acceptRequest(req, reply) {
+    async acceptRequest(req, reply)
+    {
         const request_id = req.params.id;
         const data = await friendRequest.acceptRequest(request_id, req.session.user_id);
         await redisService.postMessage('newFriendshipEvent', JSON.stringify({user1: data.from_user_id, user2: data.to_user_id})); //debug
         await redisService.updateFriendsCache(data.from_user_id, data.to_user_id);
         return reply.send();    
     }
+
+
     /* 
         Endpoint to reject the friendship requests that u received
         POST - localhost:8080/friend_requests/{request_id}/reject
-        @params {HTTP Header} Authorization: Bearer <JWT> 
         @params request_id: (Long) - id of friend_request
     */
-    async rejectRequest(req, reply) {
-        const request_id = req.params.id;
-        await friendRequest.rejectRequest(request_id, req.session.user_id);
+    async rejectRequest(req, reply)
+    {
+        await friendRequest.rejectRequest(req.params.id, req.session.user_id);
         return reply.send();   
     }
+
+
     /*
         Endpoint to retrieve all friends of user, based on friend requests table
-        @params {HTTP Header} Authorization: Bearer <JWT> 
         GET -  localhost:8080/friends
     */
-    async getAllFriends(req, reply) {
+    async getAllFriends(req, reply)
+    {
         // implement caching logic in getAllFriends
         const friends = await friendRequest.getAllFriends(req.session.user_id);
         return reply.send(friends);   
--- src/User-Friends/Adapters/inbound/http/UserController.js
+++ src/User-Friends/Adapters/inbound/http/UserController.js
@@ -8,11 +8,11 @@ const exception = require('../../../Infrastructure/config/CustomException');
 
 class UserController {
 
+
     /* 
-        Endpoint for debug purposes, thats why returns too many critical data
-        GET - localhost:8080/users/{id}
-        @params {HTTP Header} Authorization: Bearer <JWT>
-        @returns list of users
+    *    Endpoint for debug purposes, thats why returns too many critical data
+    *    GET - localhost:8080/users/{id}
+    *    @returns list of users
     */
     async getAll(req, reply)
     {
@@ -20,98 +20,115 @@ class UserController {
         return reply.send(users);
     }
 
+
+    /* 
+    *    Endpoint to return a few personal data of user
+    *    GET - localhost:8080/users/me
+    *    @returns id and nickname
+    */
     async getMyData(req, reply)
     {
         const users = await userService.findById(req.session.user_id);
         return reply.send({id: users[0].user_id, nickname: users[0].name});
     }
 
+
+
     /* 
-        Get by id endpoint
-        GET - localhost:8080/users/{id}
-        @params {id: (long)} - user_id that is being searched
-        @returns list of users
+    *    Get by id endpoint
+    *    GET - localhost:8080/users/{id}
+    *    @params {id: (long)} - user_id that is being searched
+    *    @returns list of users
     */
-    async getById(req, reply) {
+    async getById(req, reply)
+    {
         const user = await userService.findById(req.params.id);
         return reply.send(user);
     }
 
+
     /* 
-        Endpoint to create user
-        POST - localhost:8080/users
-        @params name: (string)
-        @params username: (string)
-        @params email: (string)
-        @params password_hash: (string) - pass that will be converted to hash
-        */
-    async createUser(req, reply) {
+    *    Endpoint to create user
+    *    POST - localhost:8080/users
+    *    @params name: (string)
+    *    @params username: (string)
+    *    @params email: (string)
+    *    @params password_hash: (string) - pass that will be converted to hash
+    */
+    async createUser(req, reply)
+    {
         const result = await userService.createUser(req.body); // bad pratice but i can make a dto later
         if (!result) throw exception('Failed to create user!', 400);
         return reply.code(201).send();
     }
 
 
-    // todo: safer way to upload files, cause it saves the file
-    // todo: in the server, even if the user is not authenticated
-    // todo: or the request fails
     /* 
-        Endpoint to upload user avatar as multipart/form-data
-        POST - localhost:8080/users/upload-avatar
-        @params {HTTP Header} Authorization: Bearer <JWT> 
-        @params avatar: (file) - file to be uploaded
+    *    Endpoint to upload user avatar as multipart/form-data
+    *    POST - localhost:8080/users/upload-avatar
+    *    @params avatar: (file) - file to be uploaded
     */
     async uploadAvatar(req, reply)
     {
-        const file = req.file;
-        if (!file) throw exception('File not found!', 400);
-        await userService.uploadAvatar(file.path, req.session.user_id);
+        if (!req.file) throw exception('File not found!', 400);
+        await userService.uploadAvatar(req.file.path, req.session.user_id);
         return reply.send();
     }
 
-    async updateName(req, reply)
-    {
-        const body = req.body;
-        await userService.updateName({name: body.name, id: req.session.user_id});
-        return reply.send();
-    }
-
-    async updatePassword(req, reply)
-    {
-        const body = req.body;
-        await userService.updatePassword({new_password: body.password, old_password: body.old_password, id: req.session.user_id});
-        return reply.send();
-    }
 
     /* 
-        Endpoint to be able to get the static avatar picture from the server
-        GET - localhost:8080/users/avatar/{id}
-        @params {HTTP Header} Authorization
-        @params {id: (long)} - user_id that is being searched
-        @returns {file: (file)} - file to be downloaded
+    *    Endpoint to update users name
+    *    PUT - localhost:8080/users/name
+    *    @params name: (string)
     */
-    // todo: I will let this here for now, but in the future
-    // todo: when the frontend is ready, i finish this to be more accurate
-    async getAvatar(req, reply) {
+    async updateName(req, reply)
+    {
+        await userService.updateName({name: req.body.name, id: req.session.user_id});
+        return reply.send();
+    }
+
+
+    /* 
+    *    Endpoint to update users password
+    *    PUT - localhost:8080/user/password
+    *    @params name: (string)
+    */
+    async updatePassword(req, reply)
+    {
+        await userService.updatePassword({new_password: req.body.password,
+                        old_password: req.body.old_password, id: req.session.user_id});
+        return reply.send();
+    }
+
+
+    /* 
+    *    Endpoint to be able to get the static avatar picture from the server
+    *    GET - localhost:8080/users/avatar/{id}
+    *    @params {id: (long)} - user_id that is being searched
+    *    @returns {file: (file)} - base64
+    */
+    async getAvatar(req, reply)
+    {
         const user = await userService.findById(req.params.id);
-        if (!user || Object.keys(user).length === 0) throw exception('User not found!', 404);
+        if (!user || user.length === 0) throw exception('User not found!', 404);
         const imagePath = user[0].user_image;
         if (!imagePath) throw exception('Image not defined!', 204);
         const stream = fs.createReadStream(imagePath);
         return reply.type('image').send(stream);
     }
 
+
     /* 
-        Login endpoint to authenticate users and cache friendships on redis
-        POST - localhost:8080/login
-        @params {username: (string), password: (string)}
-        @returns {token: (string)} - returns the jwt token of user
-        @returns {verified: (boolean)} - returns if user is fully authenticated (true)
-        or still need  to make 2FA auth (false)
+    *    Login endpoint to authenticate users and cache friendships on redis
+    *    POST - localhost:8080/login
+    *    @params {username: (string), password: (string)}
+    *    @returns {token: (string)} - returns the jwt token of user
+    *    @returns {verified: (boolean)} - returns if user is fully authenticated (true)
+    *    or still need  to make 2FA auth (false)
     */
-    async Login(req, reply) {
-        const user = req.body;
-        const { user_id, twofa_secret } = await userService.Login(user);
+    async Login(req, reply)
+    {
+        const { user_id, twofa_secret } = await userService.Login(req.body);
         const token = await jwtService.generate(user_id);
         await redisService.postMessage('cacheFriends', JSON.stringify({ user_id: user_id }));
         if (twofa_secret) // if the user have 2FA activated
@@ -123,12 +140,14 @@ class UserController {
         return reply.send({ token: token, verified: true });
     }
 
+
     /* 
-        Endpoint to activate 2FA to user account and generate qrcode
-        POST - localhost:8080/twofa/activate
-        @params {HTTP Header} Authorization: Bearer <JWT> 
+    *    Endpoint to activate 2FA to user account and generate qrcode
+    *    POST - localhost:8080/twofa/activate
+    *    @returns QRCODE - to be used on google authenticator
     */
-    async activateTwoFactorAuth(req, reply) {
+    async activateTwoFactorAuth(req, reply)
+    {
         // todo: optimize the query later
         const user = await userService.findById(req.session.user_id);
         if (user.twofa_enabled) throw exception('2FA already activated', 400);
@@ -137,17 +156,17 @@ class UserController {
         return reply.send({qrcode: twofa.qrCodeUrl});
     }
 
+
     /* 
-        Endpoint to verify the 2FA TOKEN, after login if the user is not verified
-        will be necessary send the token from google authenticator or other to check-in
-        POST - localhost:8080/twofa/auth
-        @params {HTTP Header} Authorization: Bearer <JWT> 
-        @params token: (string) - temporary token from 2FA app, like google authenticator
+    *    Endpoint to verify the 2FA TOKEN, after login if the user is not verified
+    *    will be necessary send the token from google authenticator or other to check-in
+    *    POST - localhost:8080/twofa/auth
+    *    @params token: (string) - temporary token from 2FA app, like google authenticator
     */
-    async twofa_verify(req, reply) {
-        const body = req.body;
+    async twofa_verify(req, reply)
+    {
         const user = await userService.findById(req.session.user_id);
-        if (!await twofaService.verifyToken(user[0].twofa_secret, body.token)) throw exception('Wrong 2FA token!', 401);
+        if (!await twofaService.verifyToken(user[0].twofa_secret, req.body.token)) throw exception('Wrong 2FA token!', 401);
         const jwt = req.headers.authorization.substring(7);
         await redisService.saveSession(jwt, { user_id: req.session.user_id, twofa_verified: 1 });
         return reply.send();
