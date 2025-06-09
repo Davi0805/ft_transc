fcef04e4e05d9c4ef4c652fa27cb2159e51f817e
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-06-09 21:40:41 +0100

    frontend: refactor Chat class to manage friends and integrate WebSocket connection

--- src/Frontend/js/components/sidebar.js
+++ src/Frontend/js/components/sidebar.js
@@ -1,13 +1,24 @@
 import { getUserDataById } from "../api/getUserDataAPI.js";
 import { getUserAvatarById } from "../api/getUserAvatarAPI.js";
 import { authService } from "../services/authService.js";
-
-class Chat {
-    constructor() {
-        this.users = [];
+import { webSocketService } from "../services/webSocketService.js";
+export class Chat {
+    constructor(userID) {
+        this.friends = [];
         this.minimized = false;
+        this.openChatWindows = new Map();
+        this.userID = userID;
     }
 
+    async init() {
+        this.setToken();
+
+        webSocketService.connect(this.token, this.userID);
+
+        await this.getSidebarConversations();
+        this.insertContactsOnSidebar();
+    }
+    
     async getSidebarConversations() {
         const convs = getSelfConversations(this.token);
         if (convs.lenght == 0) return;
@@ -15,25 +26,54 @@ class Chat {
         for (const conv of convs) {
 
             const friendData = await getUserDataById(authService.getToken(), conv.friend_id);
-
             let friendAvatarURL = await getUserAvatarById(authService.getToken(), conv.friend_id);
-            if (!friendAvatarURL) friendAvatarURL = "./Assets/default/bobzao.jpg"
 
-            this.users.push({ name: friendData.name, avatar: imgURL });
+            if (!friendAvatarURL) 
+                friendAvatarURL = "./Assets/default/bobzao.jpg"
+
+            this.friends.push({ 
+                friendID: conv.friend_id,
+                friendName: friendData.name,
+                friendAvatar: friendAvatarURL
+            });
         }
     }
 
     insertContactsOnSidebar() {
 
         const chatContacts = document.querySelector('.chat-contacts');
-        this.users.forEach(user => {
-            chatContacts.insertAdjacentHTML('beforeend', `
+        this.friends.forEach(user => {
+            const contactBtn = document.createElement('button');
+            contactBtn.className = 'contact';
+            contactBtn.innerHTML =  `
                 <button class="contact">
                     <img src="${user.avatar}" width="40px" height="40px">
                     ${ this.minimized ? `<span>${user.name}</span>` : "" }
                 </button>
-                `);
+            `;
+
+            contactBtn.addEventListener('click', () => {
+                this.openChatWindow(user);
+            });
+
+            chatContacts.appendChild(contactBtn);
         });
+    }
+
+    openChatWindow(friend) {
+        if (this.openChatWindows.has(friend.id)) {
+            this.openChatWindows.get(friend.id).focus();
+            return ;
+        }
+
+        const chatWindow = new ChatWindow(friend);
+        chatWindow.open();
+
+        const originalClose = chatWindow.close.bind(chatWindow);
+        chatWindow.close = () => {
+            this.openChatWindows.delete(friend.id);
+            originalClose();
+        };
     }
 
     setMinimized(value) {
@@ -51,10 +91,7 @@ class Chat {
     getToken() {
         return this.token;
     }
-
-    
 }
 
-export Chat chatService;
 
 
