4690a612ace86b2eea7d6b39bd3836e5274e44c3
SomeMedtnerGuy <nunoventuradesousa@gmail.com>
2025-06-23 13:09:25 +0100

    Translated the current content layout to tailwind in Match center

--- src/Frontend/js/pages/matchCenter.js
+++ src/Frontend/js/pages/matchCenter.js
@@ -3,15 +3,15 @@
 export const MatchCenterPage = {
     template() {
         return `
-            <div>
-                <h2>Game Area</h2>
-                <div class="underline decoration-wavy">Game</div>
-            </div>        
+            <div class="flex flex-col items-center justify-center bg-transparent backdrop-blur-[50px] border-2 border-black/40 shadow-[0_0_10px_rgba(0,0,0,0.4)] text-white rounded-[10px] px-[40px] py-[30px]">
+                <h2>About Us</h2>
+                <div>This is the About Us Page</div>
+            </div>
         `;
     }, 
 
     // Page specifc logic as lobby initialization for example
     init () {
-        console.log('Play page loaded!');
+        console.log('Match Center page loaded!');
     }
 };
