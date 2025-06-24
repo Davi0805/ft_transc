990a9e757ea2c97c48dbdded72216d43266848af
SomeMedtnerGuy <nunoventuradesousa@gmail.com>
2025-06-24 22:29:35 +0100

    Finally added scrolling lobbies. This shit is a pain in the ass

--- src/Frontend/js/pages/matchCenter.js
+++ src/Frontend/js/pages/matchCenter.js
@@ -32,31 +32,32 @@ export const MatchCenterPage = {
         ]
 
         let lobbiesHtml = "";
-        for (let i = 0; i < lobbiesInfo.length; i++) {
+        for (let i = 0; i < 30; i++) {
             lobbiesHtml += `<tr>`;
-            Object.values(lobbiesInfo[i]).forEach(item =>{
-                lobbiesHtml += `<td class="backdrop-brightness-${i%2 === 0 ? 100 : 75} p-2">${item}</td>`
+            Object.values(lobbiesInfo[i % 2]).forEach(item =>{
+                const bdBrightness = i % 2 === 0 ? "backdrop-brightness-100" : "backdrop-brightness-75";
+                lobbiesHtml += `<td class="${bdBrightness} p-2">${item}</td>`
             })
             lobbiesHtml += `</tr>`;
         }
 
         return `
-            <div class="flex flex-col items-center justify-center bg-transparent backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-[40px] py-[30px]">
-                <h1 class="text-3xl">Match Center</h1>
-                <div id="match-center-body" class="flex flex-row">
-                    <div id="open-lobies">
-                        <h2>Open Lobbies</h2>
-                        <div id="lobies-list">
-                            <table class="w-full table-fixed">
+            <div class="flex flex-col items-center justify-center h-full bg-transparent backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-[40px] py-[30px]">
+                <h1 class="text-3xl p-3">Match Center</h1>
+                <div id="match-center-body" class="flex flex-col min-h-0">
+                    <div id="lobby-list" class="flex flex-col min-h-0">
+                        <h2 class="text-2xl p-2">Lobby list</h2>
+                        <div id="lobby-table" class="flex-1 overflow-auto">
+                            <table class="table-fixed">
                                 ${lobbyCategoriesHtml}
                                 ${lobbiesHtml}
                             </table>
                         </div>
                     </div>
-                    <div id="new-buttons" class="flex flex-col">
-                        <button type="button">New Friendly Match</button>
-                        <button type="button">New Ranked Match</button>
-                        <button type="button">New Tournament</button>
+                    <div id="new-buttons" class="flex flex-row gap-4 items-center justify-center">
+                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Friendly Match</button>
+                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Ranked Match</button>
+                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Tournament</button>
                     </div>
                 </div>
             </div>
