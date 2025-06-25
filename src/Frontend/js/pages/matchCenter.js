aeb7564f20ab0cee2c4a52b85575bc88204244a3
SomeMedtnerGuy <nunoventuradesousa@gmail.com>
2025-06-25 16:16:17 +0100

    Match center in a pretty good state visually. Logic behind buttons and stuff still misisng

--- src/Frontend/js/pages/matchCenter.js
+++ src/Frontend/js/pages/matchCenter.js
@@ -4,9 +4,9 @@ export const MatchCenterPage = {
     template() {
         const lobbyCategories = ["NAME", "HOST", "TYPE", "CAPACITY", "MODE", "MAP", "MATCH LENGTH"]
         let lobbyCategoriesHtml = "";
-        lobbyCategoriesHtml += `<tr class="backdrop-brightness-50 text-left">`;
+        lobbyCategoriesHtml += `<tr class="backdrop-brightness-60 text-left">`;
         for (let category of lobbyCategories) {
-            lobbyCategoriesHtml += `<th class="border-l border-current/50 p-2">${category}</th>`
+            lobbyCategoriesHtml += `<th class="border-l border-white/50 px-6 py-2 relative">${category}</th>`
         }
         lobbyCategoriesHtml += `</tr>`
 
@@ -21,7 +21,7 @@ export const MatchCenterPage = {
                 matchLength: 300
             },
             {
-                name: "Lelelelele",
+                name: "Lelelele",
                 host: "artuda",
                 type: "tournament",
                 capacity: "5/100",
@@ -36,28 +36,32 @@ export const MatchCenterPage = {
             lobbiesHtml += `<tr>`;
             Object.values(lobbiesInfo[i % 2]).forEach(item =>{
                 const bdBrightness = i % 2 === 0 ? "backdrop-brightness-100" : "backdrop-brightness-75";
-                lobbiesHtml += `<td class="${bdBrightness} p-2">${item}</td>`
+                lobbiesHtml += `<td class="${bdBrightness} px-6 py-2 wrap-anywhere">${item}</td>`
             })
             lobbiesHtml += `</tr>`;
         }
 
         return `
-            <div class="flex flex-col items-center justify-center h-full bg-transparent backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-[40px] py-[30px]">
+            <div class="flex flex-col items-center justify-center h-full backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-16 py-12">
                 <h1 class="text-3xl p-3">Match Center</h1>
-                <div id="match-center-body" class="flex flex-col min-h-0">
-                    <div id="lobby-list" class="flex flex-col min-h-0">
+                <div id="match-center-body" class="flex flex-col min-h-0 gap-4">
+                    <div id="lobby-list" class="flex flex-col min-h-0 gap-4">
                         <h2 class="text-2xl p-2">Lobby list</h2>
-                        <div id="lobby-table" class="flex-1 overflow-auto">
-                            <table class="table-fixed">
-                                ${lobbyCategoriesHtml}
-                                ${lobbiesHtml}
+                        <div id="lobby-table" class="block max-w-full max-h-full overflow-y-auto overflow-x-auto">
+                            <table class="w-full">
+                                <thead class="sticky top-0 z-1 backdrop-blur-xs">
+                                    ${lobbyCategoriesHtml}
+                                </thead>
+                                <tbody>
+                                    ${lobbiesHtml}
+                                </tbody>
                             </table>
                         </div>
                     </div>
-                    <div id="new-buttons" class="flex flex-row gap-4 items-center justify-center">
-                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Friendly Match</button>
-                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Ranked Match</button>
-                        <button type="button" class="backdrop-brightness-50 p-5 rounded-4xl">New Tournament</button>
+                    <div id="new-buttons" class="flex flex-row gap-4 items-center justify-center text-xl">
+                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Friendly Match</button>
+                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Ranked Match</button>
+                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Tournament</button>
                     </div>
                 </div>
             </div>
