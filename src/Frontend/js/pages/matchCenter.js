56ab27abac252910a5d69d588c687bb3a156c2a6
SomeMedtnerGuy <nunoventuradesousa@gmail.com>
2025-06-25 18:29:01 +0100

    removed backdrop almost entirely from match center page

--- src/Frontend/js/pages/matchCenter.js
+++ src/Frontend/js/pages/matchCenter.js
@@ -35,7 +35,7 @@ export const MatchCenterPage = {
         for (let i = 0; i < 30; i++) {
             lobbiesHtml += `<tr>`;
             Object.values(lobbiesInfo[i % 2]).forEach(item =>{
-                const bdBrightness = i % 2 === 0 ? "backdrop-brightness-100" : "backdrop-brightness-75";
+                const bdBrightness = i % 2 === 0 ? "bg-gray-900/0" : "bg-gray-900/25";
                 lobbiesHtml += `<td class="${bdBrightness} px-6 py-2 wrap-anywhere">${item}</td>`
             })
             lobbiesHtml += `</tr>`;
@@ -48,11 +48,11 @@ export const MatchCenterPage = {
                     <div id="lobby-list" class="flex flex-col min-h-0 gap-4">
                         <div class="flex flex-row justify-between">
                             <h2 class="text-2xl p-2">Lobby list</h2>
-                            <button type="button" class="backdrop-brightness-60 p-2 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">Refresh</button>
+                            <button type="button" class="bg-gray-900/50 p-2 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">Refresh</button>
                         </div>
-                        <div id="lobby-table" class="block max-w-full max-h-full overflow-y-auto overflow-x-auto">
+                        <div id="lobby-table" class="block max-w-full max-h-full rounded-2xl overflow-y-auto">
                             <table class="w-full">
-                                <thead class="sticky top-0 z-1 backdrop-blur-xs">
+                                <thead class="sticky top-0 z-1 bg-gray-900/75">
                                     ${lobbyCategoriesHtml}
                                 </thead>
                                 <tbody>
@@ -62,9 +62,9 @@ export const MatchCenterPage = {
                         </div>
                     </div>
                     <div id="new-buttons" class="flex flex-row gap-4 items-center justify-center text-xl">
-                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Friendly Match</button>
-                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Ranked Match</button>
-                        <button type="button" class="backdrop-brightness-60 p-5 rounded-4xl hover:backdrop-brightness-0 active:backdrop-brightness-80">New Tournament</button>
+                        <button type="button" class="bg-gray-900/50 bg-opacity-60 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Friendly Match</button>
+                        <button type="button" class="bg-gray-900/50 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Ranked Match</button>
+                        <button type="button" class="bg-gray-900/50 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Tournament</button>
                     </div>
                 </div>
             </div>
@@ -76,3 +76,76 @@ export const MatchCenterPage = {
         console.log('Match Center page loaded!');
     }
 };
+
+
+/* MATCH CENTER */
+/*
+    const lobbyCategories = ["NAME", "HOST", "TYPE", "CAPACITY", "MODE", "MAP", "MATCH LENGTH"]
+        let lobbyCategoriesHtml = "";
+        lobbyCategoriesHtml += `<tr class="backdrop-brightness-60 text-left">`;
+        for (let category of lobbyCategories) {
+            lobbyCategoriesHtml += `<th class="border-l border-white/50 px-6 py-2 relative">${category}</th>`
+        }
+        lobbyCategoriesHtml += `</tr>`
+
+        const lobbiesInfo = [
+            {
+                name: "Blobby Lobby",
+                host: "ndo-vale",
+                type: "friendly",
+                capacity: "6/8",
+                mode: "Classic",
+                map: "4-sided-teams",
+                matchLength: 300
+            },
+            {
+                name: "Lelelele",
+                host: "artuda",
+                type: "tournament",
+                capacity: "5/100",
+                mode: "Modern",
+                map: "1v1-small",
+                matchLength: 150
+            }
+        ]
+
+        let lobbiesHtml = "";
+        for (let i = 0; i < 30; i++) {
+            lobbiesHtml += `<tr>`;
+            Object.values(lobbiesInfo[i % 2]).forEach(item =>{
+                const bdBrightness = i % 2 === 0 ? "bg-gray-900/0" : "bg-gray-900/25";
+                lobbiesHtml += `<td class="${bdBrightness} px-6 py-2 wrap-anywhere">${item}</td>`
+            })
+            lobbiesHtml += `</tr>`;
+        }
+
+        return `
+            <div class="flex flex-col items-center justify-center h-full backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-16 py-12">
+                <h1 class="text-3xl p-3">Match Center</h1>
+                <div id="match-center-body" class="flex flex-col min-h-0 gap-4">
+                    <div id="lobby-list" class="flex flex-col min-h-0 gap-4">
+                        <div class="flex flex-row justify-between">
+                            <h2 class="text-2xl p-2">Lobby list</h2>
+                            <button type="button" class="bg-gray-900/50 p-2 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">Refresh</button>
+                        </div>
+                        <div id="lobby-table" class="block max-w-full max-h-full rounded-2xl overflow-y-auto">
+                            <table class="w-full">
+                                <thead class="sticky top-0 z-1 bg-gray-900/75">
+                                    ${lobbyCategoriesHtml}
+                                </thead>
+                                <tbody>
+                                    ${lobbiesHtml}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div id="new-buttons" class="flex flex-row gap-4 items-center justify-center text-xl">
+                        <button type="button" class="bg-gray-900/50 bg-opacity-60 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Friendly Match</button>
+                        <button type="button" class="bg-gray-900/50 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Ranked Match</button>
+                        <button type="button" class="bg-gray-900/50 p-5 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">New Tournament</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    },
+*/
