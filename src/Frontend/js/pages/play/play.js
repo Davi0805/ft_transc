9d7f531bdd97b719fefb56e066489d08bf58dd0d
Nuno Vale <ndo-vale@c2r5s10.42porto.com>
2025-06-26 16:49:41 +0100

    organized /play code to update lobby list when refresh is pressed

--- src/Frontend/js/pages/play/play.js
+++ src/Frontend/js/pages/play/play.js
@@ -7,14 +7,14 @@ export const PlayPage = {
                     <div id="lobby-list" class="flex flex-col min-h-0 gap-4">
                         <div class="flex flex-row justify-between">
                             <h2 class="text-2xl p-2">Lobby list</h2>
-                            <button type="button" class="bg-gray-900/50 p-2 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">Refresh</button>
+                            <button id="btn-refresh" type="button" class="bg-gray-900/50 p-2 rounded-4xl hover:bg-gray-900/90 active:bg-gray-900/25">Refresh</button>
                         </div>
                         <div id="lobby-table" class="block max-w-full max-h-full rounded-2xl overflow-y-auto">
                             <table class="w-full">
-                                <thead class="sticky top-0 z-1 bg-gray-900/75">
+                                <thead id="lobbies-head" class="sticky top-0 z-1 bg-gray-900/75">
                                     
                                 </thead>
-                                <tbody>
+                                <tbody id="lobbies-body">
                                     
                                 </tbody>
                             </table>
@@ -31,6 +31,14 @@ export const PlayPage = {
     },
 
     init() {
+        const lobbiesHead = document.getElementById('lobbies-head');
+        lobbiesHead.innerHTML = this.getLobbyCategoriesHtml();
+
+        this.updateCurrentLobbiesHtml();
+
+        const buttonRefresh = document.getElementById('btn-refresh');
+        buttonRefresh.addEventListener('click', () => this.updateCurrentLobbiesHtml());
+
         const buttonNewFriendly = document.getElementById('btn-new-friendly');
         const buttonNewRanked = document.getElementById('btn-new-ranked');
         const buttonNewTournament = document.getElementById('btn-new-tournament');
@@ -39,5 +47,54 @@ export const PlayPage = {
         buttonNewTournament.addEventListener('click', () => window.router.navigateTo('/create-tournament'));
 
         
+    },
+
+    getLobbyCategoriesHtml() {
+        const lobbyCategories = ["NAME", "HOST", "TYPE", "CAPACITY", "MODE", "MAP", "MATCH LENGTH"]
+        let lobbyCategoriesHtml = "";
+        lobbyCategoriesHtml += `<tr class="backdrop-brightness-60 text-left">`;
+        for (let category of lobbyCategories) {
+            lobbyCategoriesHtml += `<th class="border-l border-white/50 px-6 py-2 relative">${category}</th>`
+        }
+        lobbyCategoriesHtml += `</tr>`
+        return lobbyCategoriesHtml;
+    },
+
+    updateCurrentLobbiesHtml() {
+        const lobbiesBody = document.getElementById('lobbies-body');
+
+        const lobbiesInfo = [
+            {
+                name: "Blobby Lobby",
+                host: "ndo-vale",
+                type: "friendly",
+                capacity: "6/8",
+                mode: "Classic",
+                map: "4-sided-teams",
+                matchLength: 300
+            },
+            {
+                name: "Lelelele",
+                host: "artuda",
+                type: "tournament",
+                capacity: "5/100",
+                mode: "Modern",
+                map: "1v1-small",
+                matchLength: 150
+            }
+        ] // TODO change this for a function that gets this info from database
+
+        let lobbiesHtml = "";
+        for (let i = 0; i < 6; i++) {
+            lobbiesHtml += `<tr>`;
+            Object.values(lobbiesInfo[i % 2]).forEach(item =>{
+                const bdBrightness = i % 2 === 0 ? "bg-gray-900/0" : "bg-gray-900/25";
+                lobbiesHtml += `<td class="${bdBrightness} px-6 py-2 wrap-anywhere">${item}</td>`
+            })
+            lobbiesHtml += `</tr>`;
+        }
+
+        lobbiesBody.innerHTML = lobbiesHtml;
+        console.log("Lobby list updated!")
     }
 }
