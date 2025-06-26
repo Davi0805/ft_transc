08f434dd4ce70cbdeb3316774e70fbda0ea266a0
Nuno Vale <ndo-vale@c2r4s9.42porto.com>
2025-06-26 21:54:16 +0100

    Created basic structure to alter lobby content dynamically

--- src/Frontend/js/pages/play/lobbyFriendly.js
+++ src/Frontend/js/pages/play/lobbyFriendly.js
@@ -6,6 +6,80 @@ export const LobbyFriendlyPage = {
     },
 
     init() {
+        //Set title
+        const titleElement = document.getElementById('lobby-title');
+        titleElement.textContent = "Friendly Match Lobby" //TODO: Probably should get the name of the lobby instead?
+
+
+        //Set slots
+        // -1: doesnt exist; 0: empty slot; 1+: userid
+        const slots = {
+            LEFT: {
+                front: -1,
+                back: 123
+            },
+            RIGHT: {
+                front: -1,
+                back: 0
+            },
+            TOP: {
+                front: -1,
+                back: 0
+            },
+            BOTTOM: {
+                front: -1,
+                back: 0
+            },
+        } //TODO: This has to be a function that returns this object
+
+        const teamsElement = document.getElementById('slots');
+        let slotsHtml = "";
+        for (const [team, roles] of Object.entries(slots)) {
+            slotsHtml += `<div class="flex flex-col">`;
+            slotsHtml += `<h3>${team}</h3>`
+            if (roles.front !== -1) {
+                slotsHtml += `<div class="flex flex-row>"`
+                slotsHtml += `<p>front</p>`
+                if (roles.back === 0) {
+                    slotsHtml += `<button type="button">Join</button>`
+                } else {
+                    slotsHtml += `<p>${roles.front}</p>` //TODO: Get info of player with this userid instead
+                }
+                slotsHtml += `</div>`
+            }
+            if (roles.back !== -1) {
+                slotsHtml += `<div class="flex flex-row>"`
+                slotsHtml += `<p>back</p>`
+                if (roles.back === 0) {
+                    slotsHtml += `<button type="button">Join</button>`
+                } else {
+                    slotsHtml += `<p>${roles.back}</p>` //TODO: Get info of player with this userid instead
+                }
+                slotsHtml += `</div>`
+            }
+            slotsHtml += `</div>`;
+        }
+
+        teamsElement.innerHTML = slotsHtml;
+
+
+        //Set settings
+        const settingsListingElement = document.getElementById('settings-listing')
+        const lobbySettings = {
+            type: "Friendly Match",
+            name: "Some lobby",
+            map: "1v1 medium",
+            mode: "modern",
+            length: "marathon"
+        }
+        let lobbySettingsHtml = "";
+        for (let setting in lobbySettings) {
+            lobbySettingsHtml += `<p>${setting}: ${lobbySettings[setting]}</p>`
+        }
+        settingsListingElement.innerHTML = lobbySettingsHtml;
+
+
+
         console.log('Lobby Friendly page loaded!')
     }
 }
--- src/Frontend/js/pages/play/templates/lobbyMatch.js
+++ src/Frontend/js/pages/play/templates/lobbyMatch.js
@@ -4,7 +4,7 @@ export const LobbyMatchPage = {
             <div class="flex flex-col items-center justify-center backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-16 py-12 overflow-hidden">
                 <h1 id="lobby-title" class="text-3xl p-3"></h1>
                 <div id="lobby-body" class="flex flex-row">
-                    <div id="teams" class="flex flex-col">
+                    <div id="slots" class="flex flex-col">
                     </div>
                     <div id="lobby-settings-and-buttons" class="flex flex-col">
                         <div id="lobby-settings">
