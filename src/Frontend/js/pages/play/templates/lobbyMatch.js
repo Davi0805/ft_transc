db389797b20eb2bea4070c02590f7bc5134031b0
SomeMedtnerGuy <nunoventuradesousa@gmail.com>
2025-06-30 11:36:57 +0100

    small adjustment to layout

--- src/Frontend/js/pages/play/templates/lobbyMatch.js
+++ src/Frontend/js/pages/play/templates/lobbyMatch.js
@@ -4,7 +4,7 @@ import { getLobbyOptionsHTML } from "../utils/concreteComponents.js";
 export const LobbyMatchPage = {
     template() {
         return `
-            <div class="flex flex-col items-center justify-center h-full backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-16 py-12 gap-3 overflow-hidden">
+            <div class="flex flex-col items-center h-full max-h-[650px] justify-center backdrop-blur-3xl border-2 border-black/40 shadow-sm text-white rounded-lg px-16 py-12 gap-3 overflow-hidden">
                 <h1 id="lobby-title" class="text-3xl p-2"></h1>
                 <h3 id="lobby-subtitle" class="text-xl p-1"></h3>
                 <div id="lobby-body" class="flex flex-row w-full min-h-0 gap-3">
