a3bfac02e9ff43d064819d026c8dec71fee47a91
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-07-10 19:55:22 +0100

    feat(settings): enhance settings layout with new password form and 2FA options

--- src/Frontend/code/pages/settings/settings.ts
+++ src/Frontend/code/pages/settings/settings.ts
@@ -7,9 +7,9 @@ export const SettingsPage = {
     const avatar = authService.userAvatar;
 
     return `
-                  <div id="settings-wrapper" class="flex rounded-xl bg-gradient-to-b from-blue-500 to-neutral-900 shadow-2xl shadow-black border-y border-black text-white">
+                  <div id="settings-wrapper" class="relative flex w-[800px] h-[450px] rounded-xl bg-gradient-to-b from-blue-500 to-neutral-900 shadow-2xl shadow-black border-y border-black text-white">
                     <!-- SIDEBAR -->
-                    <div id="settings-sidebar" class="relative flex w-50 flex-col items-center border-white/20 pt-4 text-white">
+                    <div id="settings-sidebar" class=" flex w-52 min-w-32 flex-col items-center border-white/20 pt-4 text-white">
                       <!-- INFO USER -->
                       <div class="user flex flex-col items-center">
                         <img class="user-avatar mb-2 h-16 w-16 rounded-full border-2 border-sky-300" src="${avatar}" alt="User avatar" />
@@ -32,8 +32,11 @@ export const SettingsPage = {
                       </div>
                     </div>
 
+                    <!-- Content shadow -->
+                    <div class="pointer-events-none absolute top-0 left-52 h-full w-2 bg-gradient-to-r from-black/10 to-transparent"></div>
+
                     <!-- Settings body -->
-                    <div id="settings-content" class="relative flex flex-1 flex-col p-8 text-white">
+                    <div id="settings-content" class=" flex flex-1 flex-col p-8 text-white">
                       ${SettingsPage.getAccountHTML()}
                     </div>
                   </div>
@@ -45,11 +48,7 @@ export const SettingsPage = {
     const nickname = authService.userNick;
     const email = authService.userEmail;
 
-    // <div class="settings-wrapper flex bg-gradient-to-b from-blue-500 to-neutral-900">
           return `
-                    <!-- Content shadow -->
-                    <div class="pointer-events-none absolute top-0 left-0 h-full w-2 bg-gradient-to-r from-black/10 to-transparent"></div>
-
                     <h1 class="mb-6 text-4xl font-bold">Settings</h1>
                     <h2 class="mb-4 border-t border-white/20 pt-4 text-2xl font-semibold">Account</h2>
 
@@ -85,7 +84,35 @@ export const SettingsPage = {
 
   getSecurityHTML(): string  {
     return `
-    
+          <h1 class="mb-6 text-4xl font-bold">Settings</h1>
+          <h2 class="mb-4 border-t border-white/20 pt-4 text-2xl font-semibold">Security</h2>
+
+          <form id="pass-change" class="flex flex-col flex-1 space-y-6">
+            <!-- Old Password -->
+            <div class="old-pass flex items-center space-x-3">
+              <label for="old-pass" class="w-20 font-semibold text-center">Current password</label>
+              <input id="old-pass" type="password" class="h-11 rounded-3xl border-2 border-white/20 bg-white px-[20px] py-[20px] pr-[45px] text-base font-medium text-black caret-black outline-none focus:border-transparent focus:ring-2 focus:ring-blue-300  transition-all duration-200 ease-in" />
+            </div>
+
+            <!-- New Password -->
+            <div class="flex items-center space-x-3">
+              <label for="new-pass" class="w-20 font-semibold text-center">New password</label>
+              <input id="new-pass" type="password" class="h-11 rounded-3xl border-2 border-white/20 bg-white px-[20px] py-[20px] pr-[45px] text-base font-medium text-black caret-black outline-none focus:border-transparent focus:ring-2 focus:ring-blue-300  transition-all duration-200 ease-in" />
+            </div>
+            
+            <div id="save-btn" class="mt-auto flex justify-end ">
+             <button type="submit" class="rounded-lg bg-blue-600 px-6 py-2 font-semibold text-white transition-all duration-100 hover:bg-blue-700 active:scale-85">Save</button>
+            </div>
+          </form>
+
+          <!-- Enable/Disable 2FA -->
+          <div class="flex pt-2 ">
+            <h3 class="text-l font-semibold">Two Factor Authentication</h3>
+             <button class="ml-6 rounded-lg bg-blue-600 px-6 py-1 font-semibold text-white transition-all duration-100 hover:bg-blue-700 active:scale-85">
+             ${authService.getHas2FA() ? "Turn off" : "Turn on"}
+             </button>
+          </div>
+
     `;
   },
 
