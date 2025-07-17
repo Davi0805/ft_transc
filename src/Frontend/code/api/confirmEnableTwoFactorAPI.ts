9043087db77fd34741d3a25ad032593a26d920bb
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-07-17 14:59:07 +0100

    feat(twoFactorAuth): add confirmation function for 2FA code and fix enableTwoFactor function endpoint

--- src/Frontend/code/api/confirmEnableTwoFactorAPI.ts
+++ src/Frontend/code/api/confirmEnableTwoFactorAPI.ts
@@ -1,24 +1,23 @@
 import { authService } from "../services/authService";
 
-
-export async function enableTwoFactor(): Promise<string> {
+export async function confirmTwoFactorCode(code: string): Promise<void> {
   try {
-    const response = await fetch("http://localhost:8080/twofa/activate", {
+    const response = await fetch("http://localhost:8080/twofa/activate/confirm", {
       method: "POST",
       headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${authService.getToken()}`,
       },
+      body: JSON.stringify({ token: code }),
     });
 
     if (!response.ok) {
-      const errorMessage = `2FA code failed with status ${response.status}`;
+      const errorMessage = `2FA confirmation code failed with status ${response.status}`;
       const error = new Error(errorMessage);
       (error as any).status = response.status;
       throw error;
     }
-    
-    const data = await response.json();
-    return data.qrcode;
+    return; // sucess
   } catch (error) {
     throw error;
   }
