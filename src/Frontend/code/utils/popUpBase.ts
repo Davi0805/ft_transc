d3f14e886550a6c120b85e70cba85e9cbc1eedd2
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-07-18 20:40:42 +0100

    feat(popup): add PopupBase class for managing popups with dynamic container creation

---
+++ src/Frontend/code/utils/popUpBase.ts
@@ -0,0 +1,38 @@
+import { authService } from "../services/authService";
+
+export interface PopupConfig {
+    title: string;
+    description: string;
+    autoClose?: boolean;
+}
+
+export type PopupType = "success" | "error" | "warning";
+
+
+export class PopupBase {
+    private container: HTMLElement;
+
+    constructor(containerId: string = "popupContainer") {
+        this.container = document.getElementById(containerId) || this.createContainer(); 
+    }
+    private createContainer(): HTMLElement {
+        const container = document.createElement('div');
+        container.id = "popupContainer";
+        container.classList.add(authService.isAuthenticated ? 'right-52' : 'right-5');
+        document.querySelector("main")?.appendChild(container);
+        return container;
+    }
+
+    protected addPopup(element: HTMLElement): void {
+        this.container.appendChild(element);
+    }
+
+    protected removePopup(element: HTMLElement): void {
+        element.style.transform = "translateX(100%)";
+        element.style.opacity = '0';
+
+        setTimeout(() => {
+            element.remove();
+        }, 300);
+    }
+}
