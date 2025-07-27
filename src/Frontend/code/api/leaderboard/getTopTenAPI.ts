c4bb87777c7f765127acd0ff1c9465390f57697e
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-07-27 22:35:03 +0100

    feat(api):: Move PlayerStats interface to a separate file

--- src/Frontend/code/api/leaderboard/getTopTenAPI.ts
+++ src/Frontend/code/api/leaderboard/getTopTenAPI.ts
@@ -1,12 +1,4 @@
-export interface PlayerStats {
-  id: number; // userID
-  username: string;
-  rank?: number; // Rank of the player in the leaderboar
-  wins: number;
-  losses: number;
-  points: number; // Points of the player in the leaderboard
-}
-
+import { PlayerStats } from "./types/PlayerStatsInterface";
 /**
  * @brief
  *
