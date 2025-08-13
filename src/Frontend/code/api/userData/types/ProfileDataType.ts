45ee2e7de58242f292ac719d5d5e95c82ad65a5b
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-08-13 18:25:08 +0100

    feat(Profile data): added api call for fetching userdata for profile and its corresponding type

---
+++ src/Frontend/code/api/userData/types/ProfileDataType.ts
@@ -0,0 +1,9 @@
+export interface ProfileDataType {
+    user_id: number;
+    nickname: string;
+    username: string;
+    rating: number;
+    friendsCount: number;
+    is_friend: boolean;
+    //todo is_blocked: boolean;
+}
