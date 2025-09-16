52a5f3eec80ce258b9731d26d6a995689275f548
Artur-2k <arturcarvalho0507@gmail.com>
2025-09-16 17:59:48 +0100

    refactor header component styles with Tailwind CSS and remove unused CSS file

--- src/Frontend/code/components/header.ts
+++ src/Frontend/code/components/header.ts
@@ -15,22 +15,23 @@ class Header {
   createHeaderElement(): void {
     const headerElement = document.createElement("header");
     headerElement.id = "header";
-
+    headerElement.className = "fixed top-0 left-0 right-0 z-10 h-[60px] py-2 px-6 \
+                              flex gap-6 bg-[#172332] border-b-2 border-b-[#00000066]";
     headerElement.innerHTML = `
-      <div class="left flex flex-center flex-shrink-0 min-w-0">
-        <a href="/" class="flex center gap-2 site-home" data-link>
+      <div class="flex items-center justify-center flex-0">
+        <a href="/" class="flex items-center justify-center gap-2" data-link>
           <img
-            class="logo"
+            class="mr-6 h-10 w-auto"
             src="../../Assets/42PortoLogo.png"
             alt="42 Porto logo"
             draggable="false"
           />
-          <span class="site-title">ft_transcendence</span>
+          <span class="text-xl font-bold text-white">ft_transcendence</span>
         </a>
       </div>
 
-      <div class="right flex flex-center gap-4 min-w-0">
-        <nav id="header-nav" class="flex flex-center gap-6">
+      <div class="flex items-center justify-end flex-1 gap-4 ">
+        <nav id="header-nav" class="flex items-center justify-center gap-6">
           <a
             id="home-link"
             href="/"
@@ -53,26 +54,25 @@ class Header {
         <div id="user-in" class="user-in flex gap-16"></div> -->
         </nav>
 
-        <div class="language-selector flex flex-center flex-dir-col">
-          <button class="current-language flex flex-center">
+        <div class="language-selector flex flex-col justify-center items-center relative">
+          <button class="current-language flex justify-center items-center">
             <img
               src="https://flagcdn.com/w20/gb.png"
+              class="h-3 w-5 rounded-sm overflow-hidden hover:scale-125 transition-all duration-200 ease-in-out"
               data-lang="uk"
-              alt="United Kingdom"
-              flag
+              alt="United Kingdom flag"
               draggable="false"
             />
             <img
-              class="flex flex-dir flex-center"
+              class="h-5 w-5 rounded-lg overflow-hidden"
               src="../../Assets/icons/drop-down.svg"
-              alt="drop-down icon"
-              flag
+              alt="drop-down icon flag"
               draggable="false"
             />
           </button>
 
-          <div class="lang-options" style="display: none">
-            <ul class="flex flex-dir-col flex-center">
+          <div class="lang-options absolute -left-1 top-full w-10 rounded-md bg-[rgb(23,35,50)] border-2 border-black/40" style="display: none">
+            <ul class="flex flex-col justify-center items-center">
               <li>
                 <button class="lang-option">
                   <img
@@ -247,15 +247,15 @@ class Header {
       loggedIn = this.createElement("div", "user-in", "user-in flex gap-4");
 
       loggedIn.innerHTML = `
-        <div class="profile-container flex flex-center gap-8">
-          <a id="profile-link" href="/profile" class="profile nav-link" data-link>${nickname}</a>
-          <img class="profile-avatar" src="${avatarURL}" alt="user profile picture" draggable="false">
-        </div>
-        <a id="settings-link" href="/settings" data-link class="settings flex flex-center nav-link">
-          <img src="./Assets/icons/settings.svg" alt="settings icon" draggable="false">
+        <a id="profile-link" href="/profile" class="flex justify-center items-center gap-4 nav-link  transition-all duration-200 ease-in-out" data-link>
+          <span>${nickname}</span>
+          <img class="profile-avatar w-8 h-8 rounded-full border-2 border-white shadow-md shadow-[#333]" src="${avatarURL}" alt="user profile picture" draggable="false">
         </a>
-        <button id="logout" class="logout">
-          <img src="./Assets/icons/logout.svg" alt="logout icon" draggable="false">
+        <a id="settings-link" href="/settings" data-link class="settings flex justify-center items-center nav-link">
+          <img class="w-6 h-6 hover:scale-125 transition-all duration-200 ease-in-out" src="./Assets/icons/settings.svg" alt="settings icon" draggable="false">
+        </a>
+        <button id="logout" class="logout flex justify-center items-center">
+          <img class="w-6 h-6 hover:scale-125 transition-all duration-200 ease-in-out" src="./Assets/icons/logout.svg" alt="logout icon" draggable="false">
         </button>
       `;
 
