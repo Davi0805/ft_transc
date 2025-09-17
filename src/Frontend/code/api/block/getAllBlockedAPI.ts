4474bff810808e932df19ffe8ad1b9fc22504698
Artur-2k <arturcarvalho0507@gmail.com>
2025-09-17 13:14:39 +0100

    fix: correct URL format in blockByUserName API and add getBlockedUsers API function

--- src/Frontend/code/api/block/getAllBlockedAPI.ts
+++ src/Frontend/code/api/block/getAllBlockedAPI.ts
@@ -1,8 +1,14 @@
 import { authService } from "../../services/authService";
 
+export interface BlockedStatus {
+  "id": number; // todo check what id is this
+  "from_user_id": number; // the user who blocked
+  "blocked_user_id": number; // the blocked user
+}
+
 /**
  * @brief
- * Blocks the specified user by username.
+ * Fetches all blocked users for the authenticated user.
  *
  * Requires a valid authentication token from authService.
  *
@@ -10,25 +16,21 @@ import { authService } from "../../services/authService";
  *
  * @todo Can be further implemented a logic for error handling
  *
- * @param username - The username of the user to send a friend request to.
- * @returns A promise that resolves when the friend request has been created.
+ * @returns A promise that resolves to an array of FriendRequest objects.
  * @throws {Error} If the authentication token is missing or the API request fails.
- *
  */
-export async function blockByUserName(
-  username: string
-): Promise<void> {
+export async function getBlockedUsers(): Promise<BlockedStatus[]> {
   try {
     if (!authService.isUserAuthenticated()) {
-      const errorMessage: string = `DEBUG: No authToken at blockByUserName`;
+      const errorMessage: string = `DEBUG: No authToken at getBlockedUsers`;
       const error: Error = new Error(errorMessage);
       throw error;
     }
 
     const response = await fetch(
-      `http://localhost:8080/friend_requests/${username}/blockbyusername`,
+      `http://localhost:8080/friend_requests/blocked`,
       {
-        method: "POST",
+        method: "GET",
         headers: {
           Authorization: `Bearer ${authService.getToken()}`,
         },
@@ -36,13 +38,12 @@ export async function blockByUserName(
     );
 
     if (!response.ok) {
-      const errorMessage: string = `DEBUG: blockByUserName failed with status ${response.status}`;
+      const errorMessage: string = `DEBUG: getBlockedUsers failed with status ${response.status}`;
       const error: Error = new Error(errorMessage);
       (error as any).status = response.status;
       throw error;
     }
-
-    return;
+    return (await response.json()) as BlockedStatus[];
   } catch (error) {
     throw error;
   }
