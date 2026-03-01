6c44f560362c113d98bd1a2f67377eb99d662a45
Artur-2k <arturcarvalho0507@gmail.com>
2026-03-01 20:35:42 +0000

    Deleted old Game folder

--- src/Game/Infrastructure/config/CustomException.js
+++
@@ -1,8 +0,0 @@
-function customException(message, statusCode)
-{
-    const error = new Error(message);
-    error.statusCode = statusCode;
-    return error;
-}
-
-module.exports = customException;
